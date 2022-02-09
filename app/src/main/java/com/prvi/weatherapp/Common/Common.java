e00d213ca419faf91f3fd85cf9884bbf3bcab010
Dejan Petrovic <dejanp775@gmail.com>
2022-02-09 04:53:36 +0100

    v1.3
    Ucitavaju se podaci sa API-a, odradjen UI

--- app/src/main/java/com/prvi/weatherapp/Common/Common.java
+++ app/src/main/java/com/prvi/weatherapp/Common/Common.java
@@ -2,8 +2,25 @@ package com.prvi.weatherapp.Common;
 
 import android.location.Location;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Common {
 
     public static final String APP_ID = "5d36b5d8c14c7600246ef24ae19620bd";
     public static Location current_location = null;
+
+    public static String convertUnixToDate(int dt) {
+        Date date = new Date(dt*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm EEE MM yyyy");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
+
+    public static String convertUnixToHour(int dt) {
+        Date date = new Date(dt*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
 }
