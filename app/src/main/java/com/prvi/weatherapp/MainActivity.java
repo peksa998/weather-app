665f6feea4f621f4782c814406dceb590aeab116
Dejan Petrovic <dejanp775@gmail.com>
2022-02-09 01:40:42 +0100

    v1.2
    Popravljeni neki bagovi

--- app/src/main/java/com/prvi/weatherapp/MainActivity.java
+++ app/src/main/java/com/prvi/weatherapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Log;
 //import android.widget.Toolbar;
 import androidx.appcompat.widget.Toolbar;
 
@@ -16,6 +17,7 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 import com.karumi.dexter.Dexter;
@@ -65,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
                                 return;
                             }
+                            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
                             fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
                         }
                     }
@@ -90,6 +93,10 @@ public class MainActivity extends AppCompatActivity {
                 setupViewPager(viewPager);
                 tabLayout = (TabLayout) findViewById(R.id.tabs);
                 tabLayout.setupWithViewPager(viewPager);
+
+                // Log
+                Log.d("Location", locationResult.getLastLocation().getLatitude() + "/" + locationResult.getLastLocation().getLongitude());
+
             }
         };
     }
